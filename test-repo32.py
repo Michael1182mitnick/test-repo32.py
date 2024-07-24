3bc97f186ed87254ec10b1ac88075962a7d7508a
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-07-23 22:31:20 -0800

    Create test-repo32.py

---
+++ test-repo32.py
@@ -0,0 +1,6 @@
+# Write a program that takes two numbers as input and prints their sum.
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+# to print their sum
+print("The Sum of",first_number,"and",second_number,"is -->", first_number + second_number)
